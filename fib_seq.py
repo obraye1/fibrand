6c825840e28adaae81647f7dad8e793a465083b1
obraye1 <73139858+obraye1@users.noreply.github.com>
2022-08-23 05:14:23 +0100

    commit
    sum the first 50 fibonacci sequence

---
+++ fib_seq.py
@@ -0,0 +1,24 @@
+def fibo(n):
+    print(1)
+    counter = 1
+    old_num = 0
+    new_num = 1
+    sum_fib = 1
+    fj = []
+    while counter < n:
+        fib = old_num + new_num
+        fj = [fib]
+        
+
+        if counter < n:
+            old_num = new_num
+            new_num = fib
+            sum_fib = sum_fib + fib
+            counter = counter + 1
+
+    print(f'The sum of the first 50 value of a Fibonacci series {sum_fib}')
+    print(fj)
+
+
+
+fibo(50)
